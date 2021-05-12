3090dbeb027b0a4d0a7fc348ded43ae4cdb2d102
JuanHdz23 <juancarloshdz23@gmail.com>
2021-05-11 19:49:25 -0700

    Commit Hola Mundo Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { CuerpoComponent } from './componentes/cuerpo/cuerpo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    CuerpoComponent
   ],
   imports: [
     BrowserModule
